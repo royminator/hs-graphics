3ec5cf10cbdb7903608486d10cd7aba4fe6fa70e
rangelsen <roy.angelsen@gmail.com>
2023-04-08 21:38:02 +0200

    Cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
-import SDL
-import Linear (V4(..))
+
 import Control.Monad (unless)
+import Lib
+import SDL
 
 main :: IO ()
 main = do
-    SDL.initialize [SDL.InitVideo]
-    window <- SDL.createWindow "hs-graphics" windowConfig
-    SDL.showWindow window
-    SDL.glCreateContext window
-    appLoop
-    SDL.destroyWindow window
-    SDL.quit
-    putStrLn "hi"
+  SDL.initialize [SDL.InitVideo]
+  window <- SDL.createWindow "hs-graphics" windowConfig
+  SDL.showWindow window
+  _ <- SDL.glCreateContext window
+  app
+  SDL.destroyWindow window
+  SDL.quit
+  someFunc
 
-appLoop :: IO ()
-appLoop = do
+app :: IO ()
+app = do
   let loop = do
         let collectEvents = do
               e <- SDL.pollEvent
@@ -25,11 +26,10 @@ appLoop = do
                 Nothing -> return []
                 Just e' -> (e' :) <$> collectEvents
         events <- collectEvents
-        let quit = any (== SDL.QuitEvent) $ map SDL.eventPayload events
-        unless quit (loop)
+        let q = any ((== SDL.QuitEvent) . SDL.eventPayload) events
+        unless q loop
   loop
 
 windowConfig :: SDL.WindowConfig
 windowConfig =
-    SDL.defaultWindow { SDL.windowGraphicsContext = SDL.OpenGLContext SDL.defaultOpenGL }
-    
+  SDL.defaultWindow {SDL.windowGraphicsContext = SDL.OpenGLContext SDL.defaultOpenGL}
