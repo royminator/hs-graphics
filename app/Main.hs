67811af54e5ad7008a68267d9f4c0f9b69d0a7c0
rangelsen <roy.angelsen@gmail.com>
2023-04-08 22:22:18 +0200

    Include OpenGL and set clear color

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 module Main (main) where
 
 import Control.Monad (unless)
+import Graphics.Rendering.OpenGL as GL
 import SDL
 
 main :: IO ()
@@ -11,15 +12,18 @@ main = do
   window <- SDL.createWindow "hs-graphics" windowConfig
   SDL.showWindow window
   _ <- SDL.glCreateContext window
-  app
+  app window
   SDL.destroyWindow window
   SDL.quit
 
-app :: IO ()
-app = do
+app :: SDL.Window -> IO ()
+app window = do
+  GL.clearColor $= Color4 1 0 0 1
+  GL.clear [ColorBuffer]
   events <- readEvents
   let q = any ((== SDL.QuitEvent) . SDL.eventPayload) events
-  unless q app
+  SDL.glSwapWindow window
+  unless q (app window)
 
 readEvents :: IO [Event]
 readEvents = do
