451e668875d438c6d58849cce3a22a8231c11f42
rangelsen <roy.angelsen@gmail.com>
2023-04-09 23:02:19 +0200

    Use shaders. No triangle tho

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,25 @@
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main (main) where
 
 import Control.Monad (unless)
+import Data.Text (pack)
+import Data.Vinyl
+import Graphics.GLUtil
 import Graphics.Rendering.OpenGL as GL
+import Graphics.VinylGL
+import Linear
 import SDL
+import System.FilePath ((</>))
+
+type Pos = "position" ::: V3 GLfloat
 
 main :: IO ()
 main = do
   SDL.initialize [SDL.InitVideo]
-  window <- SDL.createWindow "hs-graphics" windowConfig
+  window <- SDL.createWindow (pack "hs-graphics") windowConfig
   SDL.showWindow window
   _ <- SDL.glCreateContext window
   app window
@@ -22,8 +32,20 @@ app window = do
   GL.clear [ColorBuffer]
   events <- readEvents
   let q = any ((== SDL.QuitEvent) . SDL.eventPayload) events
+  d <- dunno
+  d
   SDL.glSwapWindow window
   unless q (app window)
+
+dunno :: IO (IO ())
+dunno = do
+  shader <- simpleShaderProgram ("src" </> "shaders" </> "triangle2d.vert") ("src" </> "shaders" </> "triangle2d.frag")
+  verts <- bufferVertices triangle
+  vao <- makeVAO $ do
+    enableVertices' shader verts
+    bindVertices verts
+  return . withVAO vao $
+    do currentProgram $= Just (program shader)
 
 readEvents :: IO [Event]
 readEvents = do
@@ -35,3 +57,15 @@ readEvents = do
 windowConfig :: SDL.WindowConfig
 windowConfig =
   SDL.defaultWindow {SDL.windowGraphicsContext = SDL.OpenGLContext SDL.defaultOpenGL}
+
+triangle :: [FieldRec '[Pos]]
+triangle =
+  map
+    pos
+    [ V3 (-0.5) (-0.5) 0.0,
+      V3 0.5 (-0.5) 0.0,
+      V3 0.5 0.0 0.0
+    ]
+
+pos :: V3 GLfloat -> FieldRec '[Pos]
+pos = (:& RNil) . Field
---
+++ src/shaders/triangle2d.frag
@@ -0,0 +1,6 @@
+#version 150
+out vec4 color_fsout;
+
+void main() {
+    color_fsout = vec4(0.5, 0.5, 0.5, 1.0);
+}
